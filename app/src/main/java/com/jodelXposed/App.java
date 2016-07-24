a9dd2f9342bed5ede37b78f3aeb6e8205558b404
krokofant <krokofant@users.noreply.github.com>
2016-07-24 17:11:30 +0200

    Display module version and compatibility on launch

--- app/src/main/java/com/jodelXposed/App.java
+++ app/src/main/java/com/jodelXposed/App.java
@@ -1,11 +1,13 @@
 package com.jodelXposed;
 
+import android.content.pm.PackageInfo;
 import com.jodelXposed.krokofant.hooks.JodelHooks;
 import com.jodelXposed.krokofant.utils.Settings;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 import static com.jodelXposed.krokofant.utils.Log.xlog;
+import static com.jodelXposed.krokofant.utils.Utils.getSystemContext;
 
 public class App implements IXposedHookLoadPackage {
 
@@ -14,6 +16,21 @@ public class App implements IXposedHookLoadPackage {
             return;
 
         if (lpparam.packageName.equals("com.tellm.android.app")) {
+            PackageInfo pkgInfo = getSystemContext().getPackageManager().getPackageInfo(lpparam.packageName, 0);
+            xlog(String.format("----------%n" +
+                    "Starting JodelXposed%n" +
+                    "Version %s (%d)%n" +
+                    "JodelTarget %s (%d)%n" +
+                    "JodelLocal %s (%d)%n" +
+                    "----------%n",
+                BuildConfig.VERSION_NAME,
+                BuildConfig.VERSION_CODE,
+                BuildConfig.JODEL_VERSION_NAME,
+                BuildConfig.JODEL_VERSION_CODE,
+                pkgInfo.versionName,
+                pkgInfo.versionCode
+            ));
+
             xlog("Loading settings");
             Settings settings = Settings.getInstance();
             settings.load();
---
+++ app/src/main/java/com/jodelXposed/krokofant/utils/Utils.java
@@ -0,0 +1,14 @@
+package com.jodelXposed.krokofant.utils;
+
+import android.content.Context;
+
+import static de.robv.android.xposed.XposedHelpers.callMethod;
+import static de.robv.android.xposed.XposedHelpers.callStaticMethod;
+import static de.robv.android.xposed.XposedHelpers.findClass;
+
+public class Utils {
+    public static Context getSystemContext() {
+        Object activityThread = callStaticMethod(findClass("android.app.ActivityThread", null), "currentActivityThread");
+        return (Context) callMethod(activityThread, "getSystemContext");
+    }
+}
