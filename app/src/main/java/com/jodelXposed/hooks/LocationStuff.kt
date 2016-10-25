6612f478fb1f7a859bd8c5733429054492738659
krokofant <krokofant@users.noreply.github.com>
2016-10-25 06:22:28 +0200

    More kotlin conversion

---
+++ app/src/main/java/com/jodelXposed/hooks/LocationStuff.kt
@@ -0,0 +1,25 @@
+package com.jodelXposed.hooks
+
+import android.location.Location
+
+import com.jodelXposed.utils.Options
+
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+import de.robv.android.xposed.XposedHelpers.findAndHookMethod
+
+class LocationStuff(lpparam: XC_LoadPackage.LoadPackageParam) {
+    init {
+        findAndHookMethod("com.jodelapp.jodelandroidv3.data.googleservices.location.LocationManager", lpparam.classLoader, "onLocationChanged", Location::class.java, object : XC_MethodHook() {
+            @Throws(Throwable::class)
+            override fun beforeHookedMethod(param: XC_MethodHook.MethodHookParam) {
+                if (Options.location.active) {
+                    val l = param.args[0] as Location
+                    l.latitude = Options.location.lat
+                    l.longitude = Options.location.lng
+                }
+            }
+        })
+    }
+}
