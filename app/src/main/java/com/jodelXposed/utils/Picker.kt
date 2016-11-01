d4fa622574c46bb986ae00a8cf5b01b447abed2e
krokofant <krokofant@users.noreply.github.com>
2016-11-01 21:35:53 +0100

    More kotlin

--- app/src/main/java/com/jodelXposed/utils/Picker.kt
+++ app/src/main/java/com/jodelXposed/utils/Picker.kt
@@ -1,29 +1,19 @@
 package com.jodelXposed.utils
 
+
 import android.Manifest
 import android.annotation.TargetApi
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.database.Cursor
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
-
 import com.jodelXposed.JXPreferenceActivity
-import com.jodelXposed.models.Location
-import com.schibstedspain.leku.LocationPickerActivity
-
-import java.io.File
-import java.io.FileNotFoundException
-import java.io.InputStream
-
-import id.zelory.compressor.Compressor
-
 import com.jodelXposed.utils.Log.xlog
+import com.schibstedspain.leku.LocationPickerActivity
+import java.io.FileNotFoundException
 
 
 class Picker : AppCompatActivity() {
@@ -31,13 +21,18 @@ class Picker : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (!permissionsGranted()) {
-                requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE), PERMISSION_REQUEST_CODE)
-            } else
-                getAction()
-        } else {
-            getAction()
+        when (Build.VERSION.SDK_INT) {
+            in Build.VERSION_CODES.M..Build.VERSION_CODES.CUR_DEVELOPMENT -> {
+                if (permissionsGranted())
+                    getAction()
+                else
+                    requestPermissions(
+                            arrayOf(
+                                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                            ), PERMISSION_REQUEST_CODE)
+            }
+            else -> getAction()
         }
     }
 
@@ -57,11 +52,10 @@ class Picker : AppCompatActivity() {
     }
 
     private fun galleryPicker() {
-        //        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-        //        photoPickerIntent.setType("image/*");
-        //        startActivityForResult(photoPickerIntent, GALLERY_REQUEST_CODE);
-        val i = Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        startActivityForResult(i, GALLERY_REQUEST_CODE)
+        startActivityForResult(
+                Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI),
+                GALLERY_REQUEST_CODE
+        )
     }
 
     private fun startLocationPicker() {
@@ -79,33 +73,31 @@ class Picker : AppCompatActivity() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
-        xlog("ActivityResult called")
-        if (resultCode == Activity.RESULT_OK) {
-            when (requestCode) {
-                PLACEPICKER_REQUEST_CODE -> {
-                    val op = Options
-                    op.location.lat = data.getDoubleExtra(LocationPickerActivity.LATITUDE, 0.0)
-                    op.location.lng = data.getDoubleExtra(LocationPickerActivity.LONGITUDE, 0.0)
-                    op.save()
-                    Toast.makeText(this@Picker, "Success, please refresh your feed!", Toast.LENGTH_LONG).show()
-                }
-                GALLERY_REQUEST_CODE -> {
-                    val selectedImage = data.data
-                    xlog("SelectedImage:" + selectedImage.toString())
-                    try {
-                        val inputStream = contentResolver.openInputStream(selectedImage)
-                        if (inputStream == null) {
-                            xlog("input stream for image is null")
-                        } else {
+        if (resultCode != Activity.RESULT_OK)
+            return finish()
 
-                        }
-                        val bitmap = Bitmap.loadBitmap(inputStream)
-                        Bitmap.saveBitmap(bitmap)
-                    } catch (e: FileNotFoundException) {
-                        xlog("Could not load gallery image")
-                        xlog(e.message)
+        when (requestCode) {
+            PLACEPICKER_REQUEST_CODE -> {
+                Options.location.lat = data.getDoubleExtra(LocationPickerActivity.LATITUDE, 0.0)
+                Options.location.lng = data.getDoubleExtra(LocationPickerActivity.LONGITUDE, 0.0)
+                Options.save()
+                Toast.makeText(this@Picker, "Success, please refresh your feed!", Toast.LENGTH_LONG).show()
+            }
+            GALLERY_REQUEST_CODE -> {
+                val selectedImage = data.data
+                xlog("SelectedImage:$selectedImage")
+                try {
+                    val inputStream = contentResolver.openInputStream(selectedImage)
+                    if (inputStream == null) {
+                        xlog("input stream for image is null")
+                    } else {
+
                     }
-
+                    val bitmap = Bitmap.loadBitmap(inputStream)
+                    Bitmap.saveBitmap(bitmap)
+                } catch (e: FileNotFoundException) {
+                    xlog("Could not load gallery image")
+                    xlog(e.message)
                 }
             }
         }
@@ -115,8 +107,7 @@ class Picker : AppCompatActivity() {
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         when (requestCode) {
             PERMISSION_REQUEST_CODE -> {
-                grantResults[0] == 1
-                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED) {
                     getAction()
                 } else {
                     Toast.makeText(applicationContext, "This app needs permissions in order to work", Toast.LENGTH_LONG).show()
@@ -126,8 +117,6 @@ class Picker : AppCompatActivity() {
     }
 
     companion object {
-
-
         private val GALLERY_REQUEST_CODE = 202
         private val PERMISSION_REQUEST_CODE = 201
         private val PLACEPICKER_REQUEST_CODE = 200
