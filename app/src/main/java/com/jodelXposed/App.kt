40f31436e74c628a014fa6c8fe92140ac908fb65
krokofant <krokofant@users.noreply.github.com>
2016-11-29 19:38:08 +0100

    Show toast on hooks update

--- app/src/main/java/com/jodelXposed/App.kt
+++ app/src/main/java/com/jodelXposed/App.kt
@@ -118,7 +118,8 @@ class App : IXposedHookLoadPackage, IXposedHookZygoteInit, IXposedHookInitPackag
                         dlog("Replacing local hooks with repo hooks")
                         Options.hooks = repoHooks
                         Options.save()
-                        Utils.makeSnackbarWithNoCtx(App.Companion.lpparam, "Updated hooks, please force restart Jodel", -2)
+                        Toast.makeText(AndroidAppHelper.currentApplication(), "Updated hooks, please force restart Jodel", Toast.LENGTH_LONG).show()
+
                     } else {
                         dlog("Repo hooks are of the same or older version. Not updating.")
                     }
